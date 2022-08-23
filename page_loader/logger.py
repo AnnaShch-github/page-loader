a8483b3938724f0033790fd863d5efcc57636256
AnnaCanada <annazalesova@yandex.ru>
2022-08-23 17:42:10 +0300

    adding stream=sys.stdoutin logger

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -1,12 +1,12 @@
 import logging
-
+import sys
 
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(stream=sys.stdout)
 handler.setLevel(logging.DEBUG)
 handler.setFormatter(formatter)
 
