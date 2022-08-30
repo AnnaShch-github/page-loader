26f1f7c809caae94cc778641c77654ca8e15413b
AnnaCanada <annazalesova@yandex.ru>
2022-08-30 23:16:51 +0300

    fixing linter

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -15,29 +15,31 @@ def main():
         args = parse_cli_args()
         print(download(args.link, args.output))
     except requests.HTTPError:
-        logger.error('Sorry, an HTTP error has occurred. The program will be closed')
+        logger.error('Sorry, an HTTP error has occurred.'
+                     'The program will be closed')
         sys.exit(1)
     except requests.ConnectionError:
         logger.error('Sorry, failed to establish a connection to the web site.'
-              ' Please check your internet connection.')
+                     'Please check your internet connection.')
         sys.exit(1)
     except requests.URLRequired:
-        logger.error('Sorry, an URL error has occurred. The program will be closed')
+        logger.error('Sorry, an URL error has occurred.'
+                     'The program will be closed')
         sys.exit(1)
     except requests.TooManyRedirects:
-        logger.error('Sorry, too many redirects occurred. The program will be closed')
+        logger.error('Sorry, too many redirects occurred.'
+                     'The program will be closed')
         sys.exit(1)
     except requests.Timeout:
         logger.error('Sorry, the server has not issued a response for timeout. '
-              'Please repeat.')
+                     'Please repeat.')
         sys.exit(1)
     except FileNotFoundError as error:
         logger.error(f'The system cannot find the path: {error.filename}')
         sys.exit(1)
-    except PermissionError as error2:
-        logger.error(f"Sorry, you don't have the permission to {error2.filename}")
+    except PermissionError as e:
+        logger.error(f"Sorry, you don't have the permission to {e.filename}")
         sys.exit(1)
-
 
 
 if __name__ == '__main__':
