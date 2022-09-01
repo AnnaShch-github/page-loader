92ff54db0b2ed3d01579793a1ed54f3568100cbe
AnnaCanada <annazalesova@yandex.ru>
2022-09-01 13:40:09 +0300

    change logger for print in page_loader

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -4,7 +4,6 @@ import sys
 import requests
 
 from page_loader.loader import download
-from page_loader.logger import logger
 
 from page_loader.parse_cli_args import parse_cli_args
 
@@ -15,30 +14,30 @@ def main():
         args = parse_cli_args()
         print(download(args.link, args.output))
     except requests.HTTPError:
-        logger.error('Sorry, an HTTP error has occurred.'
-                     'The program will be closed')
+        print('Sorry, an HTTP error has occurred.'
+              'The program will be closed')
         sys.exit(1)
     except requests.ConnectionError:
-        logger.error('Sorry, failed to establish a connection to the web site.'
-                     'Please check your internet connection.')
+        print('Sorry, failed to establish a connection to the web site.'
+              'Please check your internet connection.')
         sys.exit(1)
     except requests.URLRequired:
-        logger.error('Sorry, an URL error has occurred.'
-                     'The program will be closed')
+        print('Sorry, an URL error has occurred.'
+              'The program will be closed')
         sys.exit(1)
     except requests.TooManyRedirects:
-        logger.error('Sorry, too many redirects occurred.'
-                     'The program will be closed')
+        print('Sorry, too many redirects occurred.'
+              'The program will be closed')
         sys.exit(1)
     except requests.Timeout:
-        logger.error('Sorry, the server has not issued a response for timeout. '
-                     'Please repeat.')
+        print('Sorry, the server has not issued a response for timeout. '
+              'Please repeat.')
         sys.exit(1)
     except FileNotFoundError as error:
-        logger.error(f'The system cannot find the path: {error.filename}')
+        print(f'The system cannot find the path: {error.filename}')
         sys.exit(1)
     except PermissionError as e:
-        logger.error(f"Sorry, you don't have the permission to {e.filename}")
+        print(f"Sorry, you don't have the permission to {e.filename}")
         sys.exit(1)
 
 
