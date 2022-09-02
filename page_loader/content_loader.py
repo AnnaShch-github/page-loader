f9972d1428b24277e2ce0ce31630cfdc5a724640
AnnaCanada <annazalesova@yandex.ru>
2022-09-02 14:30:27 +0300

    refactor exceptions in content_loader.py

--- page_loader/content_loader.py
+++ page_loader/content_loader.py
@@ -7,21 +7,13 @@ def get_content(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-    except requests.HTTPError as http_error:
-        logger_error.error(http_error)
-        raise requests.HTTPError(http_error)
-    except requests.ConnectionError as connection_error:
-        logger_error.error(connection_error)
-        raise requests.ConnectionError(connection_error)
-    except requests.URLRequired as url_error:
-        logger_error.error(url_error)
-        raise requests.URLRequired(url_error)
-    except requests.TooManyRedirects as redirects_error:
-        logger_error.error(redirects_error)
-        raise requests.TooManyRedirects(redirects_error)
-    except requests.Timeout as timeout_error:
-        logger_error.error(timeout_error)
-        raise requests.Timeout(timeout_error)
+    except (requests.HTTPError,
+            requests.ConnectionError,
+            requests.URLRequired,
+            requests.TooManyRedirects,
+            requests.Timeout) as error:
+        logger_error.error(error)
+        raise error
     else:
         data = response.content
         return data
